64bc525d2ba3a3af224088a9bd80a9f2741e76d4
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2024-01-02 09:21:50 +0200

    Make Interface And Repo OF Generic

---
+++ Souq.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Souq.Core.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souq.Core.Repositories
+{
+    public interface IGenericRepository<T>where T : BaseEntity
+    {
+
+        public Task<IEnumerable<T>> GetAllAsyc();
+
+
+
+        Task<T> GetByIdAsync(int Id);
+
+
+
+    }
+}
---
+++ Souq.Repositorey/Repo/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Souq.Core.DataBase;
+using Souq.Core.Repositories;
+using Souq.Repositorey.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souq.Repositorey.Repo
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+
+    {
+        private readonly StoreContext context;
+
+        public GenericRepository(StoreContext Context) //Ask ClR To Make Instance Implicetly
+        {
+            context = Context;
+        }
+        public async Task<IEnumerable<T>> GetAllAsyc()
+        {
+            var AllData=await context.Set<T>().ToListAsync();
+            return AllData;
+        }
+
+        public async Task<T> GetByIdAsync(int Id)
+        {
+
+            //return await context.Set<T>().Where(x=>x.Id == Id).FirstOrDefaultAsync();// Search Remotly in All Steps
+
+            return await context.Set<T>().FindAsync(Id); // have Warning IF Id Is Null Here
+
+        }
+    }
+}

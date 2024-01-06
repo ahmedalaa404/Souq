c8b5832b92516dc8242ff93a41d9d1c1e0790153
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2024-01-06 09:46:56 +0200

    implementation Repo Basket

--- Souq.Repositorey/Repo/BasketRepo.cs
+++ Souq.Repositorey/Repo/BasketRepo.cs
@@ -5,30 +5,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Souq.Repositorey.Repo
 {
     public class BasketRepo : IBasketRepo
     {
+        private readonly IDatabase redis;
 
         public BasketRepo(IConnectionMultiplexer Redis)
         {
-            
-        }
-        public Task<bool> DeleteUpdateAsync(string Id)
+            redis = Redis.GetDatabase();
+        }   
+        public async Task<CustomerBasket> GetBasketAsync(string Id)
         {
-        
+         var Basket= await redis.StringGetAsync(Id);
+            if (Basket.IsNull)
+                return null;
+            else
+               return JsonSerializer.Deserialize<CustomerBasket>(Basket);
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string Id)
+        public async Task<bool> DeleteUpdateAsync(string Id)
         {
-         
+            return await redis.KeyDeleteAsync(Id);
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket Basket)
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket Basket)
         {
-            
+          var UpdatedOrUpdate=  await redis.StringSetAsync(Basket.Id, JsonSerializer.Serialize<CustomerBasket>(Basket), TimeSpan.FromDays(1));
+            if(UpdatedOrUpdate==true)
+            {
+                return await GetBasketAsync(Basket.Id);
+            }
+            return null;
         }
     }
 }

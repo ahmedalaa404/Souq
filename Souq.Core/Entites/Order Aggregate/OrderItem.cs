23fc28130ef2ea408e7324ef1883d669bec62f64
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2024-01-12 13:52:42 +0200

    ProductItemOrder

---
+++ Souq.Core/Entites/Order Aggregate/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using Souq.Core.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souq.Core.Entites.Order_Aggregate
+{
+    public class OrderItem:BaseEntity // In Database // Product Have U Add In Order
+    {
+
+        public ProductItemOrder Product { get; set; } //Not Have Change
+
+        public decimal Price { get; set; }// can U Have Deffirent here 
+
+        public int Quantity { get; set; }
+
+
+
+
+    }
+}
---
+++ Souq.Core/Entites/Order Aggregate/ProductItemOrder.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Souq.Core.Entites.Order_Aggregate
+{
+    [NotMapped]
+    public class ProductItemOrder
+    {
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+        public string ProductPictureUrl { get; set; }
+
+
+
+
+
+
+    }
+}

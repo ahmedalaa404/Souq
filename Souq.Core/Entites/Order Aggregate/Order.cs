62d60809d3012a4329f44fa00f4ac3704cb08c2f
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2024-01-12 14:09:41 +0200

    End Of Order

---
+++ Souq.Core/Entites/Order Aggregate/Order.cs
@@ -0,0 +1,28 @@
+﻿using Souq.Core.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Souq.Core.Entites.Order_Aggregate
+{
+    public class Order:BaseEntity
+    {
+
+        public string BuyerEmail { get; set; }
+
+
+        public DateTimeOffset  OrderDate { get; set; }=DateTimeOffset.Now;
+
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public Address ShippingAddress { get; set; } // One To One   Total of the Two Dimations
+
+
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+
+    }
+
+}
